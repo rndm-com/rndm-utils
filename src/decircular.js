66036b91b3960300274dabe98da8781ad828791c
Paul Napier <>
2019-01-21 17:20:59 +1100

    Add ability to stringify functions as part of the decircular method

--- src/decircular.js
+++ src/decircular.js
@@ -1,9 +1,11 @@
-const decircular = (input, spacer = 2) => {
+const decircular = (input, spacer = 2, options = {}) => {
   const cache = [];
   const replacer = (key, value) => {
     if (typeof value === 'object' && value !== null) {
       if (cache.indexOf(value) !== -1) return;
       cache.push(value);
+    } else if (typeof  value === 'function' && options.stringifyFunctions) {
+      return value.toString();
     }
     return value;
   };
